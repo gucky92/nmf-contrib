638b129ebc2f18145025a01a8a2e843d970d39cd
gucky92 <gucky@gucky.eu>
2021-06-23 16:01:29 +0200

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import find_packages, setup
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
-VERSION = "0.0.0"
+VERSION = "0.0.1"
 # User README.md as long description
 with open("README.md", encoding="utf-8") as f:
     README = f.read()
